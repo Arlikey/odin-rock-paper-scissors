05ada6d836a6e71b0efb95658caf67f15410b6ad
Nazar Melnik <115361939+Arlikey@users.noreply.github.com>
2026-07-02 13:40:49 +0300

    Game overhaul
    Document structure and styling for the Rock Paper Scissors game.
    Added new icons, updated styles, and improved user interface elements.
    Changed game logic for user interaction and win modal display.

--- script.js
+++ script.js
@@ -1,58 +1,103 @@
-function getComputerChoice() {
+const WINNER_SCORE = 5;
+const RULES = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
+
+let userScore = 0;
+let botScore = 0;
+
+let userChoice = "";
+let botChoice = "";
+
+const userScoreSpan = document.querySelector("#user-score");
+const botScoreSpan = document.querySelector("#bot-score");
+
+const userChoiceSpan = document.querySelector(".battle-log__user-choice");
+const botChoiceSpan = document.querySelector(".battle-log__bot-choice");
+
+const winModalTitle = document.querySelector(".win-modal__title");
+const winModalSubtitle = document.querySelector(".win-modal__subtitle");
+
+const userControls = document.querySelector(".user-controls");
+const modal = document.querySelector(".win-modal");
+const restartBtn = document.querySelector(".restart-btn");
+
+restartBtn.addEventListener("click", () => {
+  reset();
+  modal.close();
+});
+
+userControls.addEventListener("click", (event) => {
+  const btn = event.target.closest(".user-controls__btn");
+  if (!btn) return;
+
+  userChoice = btn.value;
+  botChoice = getBotChoice();
+
+  playRound(userChoice, botChoice);
+  updateInterface();
+  checkWinner();
+});
+
+function getBotChoice() {
   // returns value from 0 to 2
   let randomChoice = Math.floor(Math.random() * 3);
 
   return randomChoice == 0 ? "rock" : randomChoice == 1 ? "paper" : "scissors";
 }
 
-function getHumanChoice() {
-  return prompt("Your choice is rock, paper or scissors?").trim().toLowerCase();
-}
+function playRound(userChoice, botChoice) {
+  const isWin = RULES[userChoice] === botChoice;
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+  const isTie = userChoice === botChoice;
 
-  for (let i = 0; i < 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-  }
+  if (isTie) return;
 
-  if (humanScore > computerScore) {
-    console.log(
-      `You won! Your score is ${humanScore} and computer score is ${computerScore}`,
-    );
-  } else if (humanScore < computerScore) {
-    console.log(
-      `You lose! Your score is ${humanScore} and computer score is ${computerScore}`,
-    );
+  if (isWin) {
+    ++userScore;
   } else {
-    console.log(
-      `It's tie! Your score is ${humanScore} and computer score is ${computerScore}`,
-    );
-  }
-
-  function playRound(humanChoice, computerChoice) {
-    const rules = {
-      rock: "scissors",
-      paper: "rock",
-      scissors: "paper",
-    };
-    const isWin = rules[humanChoice] === computerChoice;
-
-    const isTie = humanChoice === computerChoice;
-
-    if (isWin) {
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-      ++humanScore;
-    } else if (isTie) {
-      console.log(`It's tie! ${humanChoice} and ${computerChoice}`);
-    } else {
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-      ++computerScore;
-    }
+    ++botScore;
   }
 }
 
-playGame();
+function checkWinner() {
+  if (userScore >= WINNER_SCORE) {
+    let title = "you win!";
+    let subtitle = `You won! Your score is ${userScore} and computer score is ${botScore}`;
+    updateModalInterface(title, subtitle);
+    modal.showModal();
+  } else if (botScore >= WINNER_SCORE) {
+    let title = "you lose!";
+    let subtitle = `You lose in this fight against machine... Your score is ${userScore} and computer score is ${botScore}`;
+    updateModalInterface(title, subtitle);
+    modal.showModal();
+  } else {
+    return;
+  }
+}
+
+function updateInterface() {
+  userScoreSpan.textContent = userScore;
+  botScoreSpan.textContent = botScore;
+
+  userChoiceSpan.textContent = userChoice;
+  botChoiceSpan.textContent = botChoice;
+}
+
+function updateModalInterface(title, subtitle) {
+  winModalTitle.textContent = title;
+  winModalSubtitle.textContent = subtitle;
+}
+
+function reset() {
+  userScore = 0;
+  botScore = 0;
+
+  userChoice = "";
+  botChoice = "";
+
+  updateInterface();
+  updateModalInterface("", "");
+}
